0e86c6e6b7fe43e5106a0542db6e32b5fa1c1bdb
Gerad Suyderhoud <gerad@swivel.com>
2009-05-20 17:55:08 -0700

    php 5.1.6 compatability, leverage new API

--- swivel6.php
+++ swivel6.php
@@ -7,7 +7,7 @@ class Swivel6 {
     $this->user = $user;
     $this->password = $password;
     $this->group = $group;
-    $this->root_url = $root_url; 
+    $this->root_url = $root_url;
   }
 
   public function charts() {
@@ -15,149 +15,225 @@ class Swivel6 {
   }
 
   public function create_chart() {
-    return $this->api_post('groups/'.$this->group.'/charts',
-      array('grid' => array('name' => 'Chart Data')));
+    return $this->api_post('groups/'.$this->group.'/charts');
   }
 
   public function update_chart($chart, $data) {
-    $this->api_put('groups/'.$this->group.'/charts/'.$chart->id,
-      array('chart' => $data));
+    $this->api_put('groups/'.$this->group.'/charts/'.$chart->id, $data);
   }
 
-  public function chart_data($chart) {
-    $grid_id = $this->get_chart_grid_id($chart);
-    return $this->get_data($grid_id);
+  public function get_chart_data($chart) {
+    return $this->parse_csv($this->req('get','groups/'.$this->group.'/charts/'.$chart->id.'.csv'));
   }
 
   public function set_chart_data($chart, $data) {
-    $cells = array();
-    foreach($data as $rownum => $row) {
-      foreach($row as $colnum => $cell) {
-        $cells[] = array("$colnum,$rownum", array('v' => $cell));
-      }
-    }
-
-    $save_list = array(array('action' => 'edit', 'cell_values' => $cells));
-    $grid_id = $this->get_chart_grid_id($chart);
-
-    $this->api_put('grids/'.$grid_id, array('tabular' => array('save_list' => json_encode($save_list))));
+    $this->update_chart($chart, array('data' => $this->create_csv($data)));
   }
 
-  public function clear_chart_data($chart) {
-    $data = $this->chart_data($chart);
-    $num_rows = count($data);
-
-    if($num_rows > 0) {
-      $grid_id = $this->get_chart_grid_id($chart);
-      $save_list = array(array('action' => 'removeRows', 'start' => 0, 'end' => $num_rows - 1));
-      $this->api_put('grids/'.$grid_id, array('tabular' => array('save_list' => json_encode($save_list))));
-    }
-  }
-
-  public function insert_chart_row($chart, $position) {
-    $grid_id = $this->get_chart_grid_id($chart);
-    $save_list = array(array('action' => 'insertRows', 'rows' => '', 'position' => $position));
-    $this->api_put('grids/'.$grid_id, array('tabular' => array('save_list' => json_encode($save_list))));
-  }
-
-  public function set_chart_row($chart, $rownum, $data) {
-    $cells = array();
-    foreach($data as $colnum => $cell) {
-      $cells[] = array("$colnum,$rownum", array('v' => $cell));
-    }
-
-    $save_list = array(array('action' => 'edit', 'cell_values' => $cells));
-    $grid_id = $this->get_chart_grid_id($chart);
-
-    $this->api_put('grids/'.$grid_id, array('tabular' => array('save_list' => json_encode($save_list))));
+  public function append_chart_data($chart, $data) {
+    $this->update_chart($chart, array('data' => $this->create_csv($data), 'mode' => 'append'));
   }
 
   private function api_get($url, $params = false) {
-    return json_decode($this->req('get', $url.'.json', false));
+    return $this->xml2object($this->req('get', $url.'.xml', $params));
   }
 
   private function api_post($url, $params = false) {
-    return json_decode($this->req('post', $url.'.json', $params));
+    return $this->xml2object($this->req('post', $url.'.xml', $params));
   }
 
   private function api_put($url, $params = false) {
-    $this->req('put', $url.'.json', $params);
+    $this->req('put', $url.'.xml', $params);
   }
 
-  private function req($type, $url, $params) {
+  private function req($type, $url, $params = false) {
     $ch = curl_init($this->root_url.'/'.$url);
-//    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch, CURLOPT_USERPWD, $this->user.':'.$this->password);
     if($type != 'get') {
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($type));
-      curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml"));
 
       if($params) {
-        $root = array_shift(array_keys($params));
-        $xml = new XmlWriter();
-        $xml->openMemory();
-        $xml->startDocument('1.0', 'UTF-8');
-        $xml->startElement($root);
-
-        $this->array2xml($params[$root], $xml);
-
-        $xml->endElement();
-        $xml_str = $xml->outputMemory(true);
-//        echo $xml_str;
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_str);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
       }
     }
 
     $resp = curl_exec($ch);
     curl_close($ch);
-//    echo $resp;
+    echo $resp;
     return $resp;
   }
 
-  private function array2xml($data, XMLWriter $xml){
-    foreach($data as $key => $value){
-      if(is_array($value)){
-        $xml->startElement($key);
-        $this->array2xml($xml, $value);
-        $xml->endElement();
-        continue;
+  private function create_csv($data) {
+    $csv = fopen('php://temp/maxmemory:'. (5*1024*1024), 'r+');
+    foreach($data as $line) {
+      fputcsv($csv, $line);
+    }
+    rewind($csv);
+    return stream_get_contents($csv);
+  }
+
+  private function parse_csv($data) {
+    $csv = fopen('php://temp/maxmemory:'. (5*1024*1024), 'r+');
+    fwrite($csv, $data);
+    rewind($csv);
+    $ar = array();
+    while(($line = fgetcsv($csv)) !== FALSE) {
+      $ar[] = $line;
+    }
+    return $ar;
+  }
+
+  # http://us3.php.net/manual/en/function.xml-parse.php#87920
+  private function xml2array($contents, $get_attributes = 1, $priority = 'tag') {
+    $parser = xml_parser_create('');
+    xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, trim($contents), $xml_values);
+    xml_parser_free($parser);
+    if (!$xml_values)
+      return; //Hmm...
+    $xml_array = array ();
+    $parents = array ();
+    $opened_tags = array ();
+    $arr = array ();
+    $current = & $xml_array;
+    $repeated_tag_index = array ();
+    foreach ($xml_values as $data)
+    {
+      unset ($attributes, $value);
+      extract($data);
+      $result = array ();
+      $attributes_data = array ();
+      if (isset ($value))
+      {
+        if ($priority == 'tag')
+          $result = $value;
+        else
+          $result['value'] = $value;
       }
-      $xml->writeElement($key, $value);
+      if (isset ($attributes) and $get_attributes)
+      {
+        foreach ($attributes as $attr => $val)
+        {
+          if ($priority == 'tag')
+            $attributes_data[$attr] = $val;
+          else
+            $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+        }
+      }
+      if ($type == "open")
+      {
+        $parent[$level -1] = & $current;
+        if (!is_array($current) or (!in_array($tag, array_keys($current))))
+        {
+          $current[$tag] = $result;
+          if ($attributes_data)
+            $current[$tag . '_attr'] = $attributes_data;
+          $repeated_tag_index[$tag . '_' . $level] = 1;
+          $current = & $current[$tag];
+        }
+        else
+        {
+          if (isset ($current[$tag][0]))
+          {
+            $current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+            $repeated_tag_index[$tag . '_' . $level]++;
+          }
+          else
+          {
+            $current[$tag] = array (
+              $current[$tag],
+              $result
+            );
+            $repeated_tag_index[$tag . '_' . $level] = 2;
+            if (isset ($current[$tag . '_attr']))
+            {
+              $current[$tag]['0_attr'] = $current[$tag . '_attr'];
+              unset ($current[$tag . '_attr']);
+            }
+          }
+          $last_item_index = $repeated_tag_index[$tag . '_' . $level] - 1;
+          $current = & $current[$tag][$last_item_index];
+        }
+      }
+      elseif ($type == "complete")
+      {
+        if (!isset ($current[$tag]))
+        {
+          $current[$tag] = $result;
+          $repeated_tag_index[$tag . '_' . $level] = 1;
+          if ($priority == 'tag' and $attributes_data)
+            $current[$tag . '_attr'] = $attributes_data;
+        }
+        else
+        {
+          if (isset ($current[$tag][0]) and is_array($current[$tag]))
+          {
+            $current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+            if ($priority == 'tag' and $get_attributes and $attributes_data)
+            {
+              $current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+            }
+            $repeated_tag_index[$tag . '_' . $level]++;
+          }
+          else
+          {
+            $current[$tag] = array (
+              $current[$tag],
+              $result
+            );
+            $repeated_tag_index[$tag . '_' . $level] = 1;
+            if ($priority == 'tag' and $get_attributes)
+            {
+              if (isset ($current[$tag . '_attr']))
+              {
+                $current[$tag]['0_attr'] = $current[$tag . '_attr'];
+                unset ($current[$tag . '_attr']);
+              }
+              if ($attributes_data)
+              {
+                $current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+              }
+            }
+            $repeated_tag_index[$tag . '_' . $level]++; //0 and 1 index is already taken
+          }
+        }
+      }
+      elseif ($type == 'close')
+      {
+        $current = & $parent[$level -1];
+      }
+    }
+    return ($xml_array);
+  }
+
+  private function array2object(&$ar) {
+    $numeric_array = true;
+    foreach($ar as $k => $v) {
+      if(is_array($v)) $ar[$k] = $this->array2object($v);
+      if(!is_numeric($k)) $numeric_array = false;
+    }
+    $vals = array_values($ar);
+
+    if(count($ar) == 1 and is_array($vals[0])) {
+      return array_shift($ar);
+    } else if($numeric_array) {
+      return $ar;
+    } else {
+     return (object) $ar;
     }
   }
 
-  private function get_data($tabular_id) {
-    $cells = $this->api_get('tabulars/'.$tabular_id.'/cells');
-    $max_row = -1;
-    $max_col = -1;
-
-    foreach($cells as $cell) {
-      if($cell->r > $max_row) $max_row = $cell->r;
-      if($cell->c > $max_col) $max_col = $cell->c;
-    }
-
-    $data = array();
-    for($r = 0; $r < $max_row + 1; $r++) {
-      $row = array();
-      $row = array_pad($row, $max_col + 1, NULL);
-      $data[] = $row;
-    }
-
-    foreach($cells as $cell) {
-      $data[$cell->r][$cell->c] = $cell->formatted;
-    }
-
-    return $data;
-  }
-
-  private function get_chart_grid_id($chart) {
-    $grid = $this->api_get('groups/'.$this->group.'/charts/'.$chart->id.'/grid');
-    return $grid->id;
+  private function xml2object($contents) {
+    $ar = $this->xml2array($contents, false);
+    $root = array_shift($ar);
+    return $this->array2object($root);
   }
 
 }
@@ -168,8 +244,6 @@ $newchart = $s6->create_chart();
 echo $newchart->name."\n";
 
 $s6->update_chart($newchart, array('title' => 'API Chart', 'name' => 'API Chart', 'description' => 'A chart made automatically with the Swivel API.'));
-
-$s6->clear_chart_data($newchart);
 $s6->set_chart_data($newchart, array(
   array('Monthly Summary','New Topics','New Posts','New Members','Most Online'),
   array('March 2009',922,12133,193,26),
@@ -180,15 +254,14 @@ $s6->set_chart_data($newchart, array(
   array('October 2008',0,0,2,1),
   array('September 2008',2,2,0,1),
   array('August 2008',14,21,9,2),
-  array('July 2008',4,9,6,4),
-));
-$s6->insert_chart_row($newchart, 1);
-$s6->set_chart_row($newchart, 1, array('April 2009',271,3372,43,25));
+  array('July 2008',4,9,6,4)));
+$s6->append_chart_data($newchart, array(array('April 2009',271,3372,43,25)));
 
-print_r($s6->chart_data($newchart));
+print_r($s6->get_chart_data($newchart));
 echo "Created chart at https://business.swivel.com/charts/".$newchart->id."\n";
 
 $charts = $s6->charts();
+
 foreach($charts as $chart) {
   echo $chart->name.": https://business.swivel.com/charts/".$chart->id."\n";
 }
